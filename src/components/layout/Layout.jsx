3e7e58d0004a7ef2b7d0c6006a40f676fb3ed6a4
tmr1n <163327345+tmr1n@users.noreply.github.com>
2025-07-25 17:05:31 +0200

    Header was lost

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,11 +1,13 @@
 import './layout.module.scss'
 
 import Header from './header/Header.jsx'
+import Home from '@/components/screens/home/Home.jsx'
 
 const Layout = () => {
 	return (
 		<div>
 			<Header />
+			<Home />
 		</div>
 	)
 }
--- src/components/screens/home/Home.jsx
+++ src/components/screens/home/Home.jsx
@@ -1,28 +1,17 @@
-import { useState } from 'react'
-
-import Layout from '../../layout/Layout'
+import styles from './home.module.scss'
 
 function Home() {
-	const [count, setCount] = useState(0)
-
 	return (
-		<Layout>
-			<div className='all'></div>
-			<div className='Home'>
-				<h1>Vite + React</h1>
-				<div className='card'>
-					<button onClick={() => setCount(count => count + 1)}>
-						count is {count}
-					</button>
-					<p>
-						Edit <code>src/Home.jsx</code> and save to test HMR
-					</p>
-				</div>
-				<p className='read-the-docs'>
-					Click on the Vite and React logos to learn more
-				</p>
-			</div>
-		</Layout>
+		<section className={styles.home}>
+			<h1> Как вы хотите заниматься? </h1>
+			<h2>
+				Освойте любой изучаемый материал с помощью интерактивных карточек,
+				пробных тестов и учебных активностей в Quizlet.
+			</h2>
+			<button className={styles.buttonBluee} onClick={() => {}}>
+				Зарегистрироваться бесплатно
+			</button>
+		</section>
 	)
 }
 
